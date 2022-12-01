9b2969e0b8f86809a898b9fd6bbdf9a0201f4655
Pranavbala Ganesh <47910683+Pranavbala03@users.noreply.github.com>
2022-12-02 01:50:01 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import logo from './assets/logo.svg'
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <div>
+          <span></span>
+        </div>
+       <h1 className='Main'>
+        coming soon...
+       </h1>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        />
+      </header>
+    </div>
+  );
+}
+
+export default App;
