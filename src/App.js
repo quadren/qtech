26dd07b8a93b769330fc13731180acc8080a8a6e
Pranavbala Ganesh <47910683+Pranavbala03@users.noreply.github.com>
2022-12-04 15:24:07 +0530

    more changes
    c

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './assets/logo.svg'
-import './App.css';
+import logo from "./assets/logo.svg";
+import "./App.css";
+import { TwitterLogo } from "phosphor-react";
+
+// function randomColor() {
+//   var color = '#';
+//   var colorCode = ['540CF2','FA474A','F259C7','77E00DB']; // colors
+//   var className = document.getElementsByClassName("change"); // class name to random color
+//   var i;
+//   color += colorCode[Math.floor(Math.random() * colorCode.length)];
+// for (var i = 0; i < className.length; i ++) {
+//     className[i].style.backgroundColor = color; }
+// }
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <div>
-          <span></span>
+  
+      <body className="App" >
+        <div className="content">
+        <div className="App-content">
+          <div className="App-logo">
+            <img src={logo} className="App-logo" alt="logo" />
+          </div>
+          <div className="container">
+            <h1 className="Text">Working on it.</h1>
+            <a href="https://twitter.com/quadrentech">
+              <button className="button">
+                <div className="button-text">
+                  <TwitterLogo size={14} weight="fill" />
+                  <span className="space" />
+                  Follow along on Twitter      
+                  </div>        
+              </button>{" "}
+            </a>
+            </div>
+            </div>
         </div>
-       <h1 className='Main'>
-        coming soon . . . 
-       </h1>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        />
-      </header>
-    </div>
+        
+      </body>
+  
   );
 }
 
